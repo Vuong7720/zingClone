fef0e1be66eb26d715d461573e1a2587ede6fa0f
Vuong7720 <91041619+Vuong7720@users.noreply.github.com>
2023-05-14 21:27:13 +0700

    Add files via upload

---
+++ zing/js/home.js
@@ -0,0 +1,56 @@
+
+
+
+// var leftItems = document.getElementById('leftItems');
+// console.log(leftItems);
+// var leftItem =document.querySelectorAll("left-on-item");
+// leftItem.addEventListener("click", function(){
+//     leftItem.classList.toggle("active");
+// });
+
+
+// var find = document.getElementById("find");
+// find.addEventListener("click", Findfunction);
+// function Findfunction() {
+//     document.getElementById("history").style.display = "block";
+//     document.getElementById("fake_after").style.display = "block";
+// }
+
+
+
+
+// var leftItem = document.getElementsByClassName("left-on-item");
+
+// document.getElementById("input-head").addEventListener("click", function() {
+//     var card2 = document.getElementById("history")
+//     card2.classList.add('hidden1');
+//   });
+  
+//   document.addEventListener("click", function(event) {
+//     var card2 = document.getElementById("history");
+//     var card3 = document.getElementById("fake_after");
+//     var card1 = document.getElementById("find");
+//     var targetElement = event.target;
+    
+//     if (targetElement !== card1 && !card2.contains(targetElement)) {
+//       card2.classList.remove("hidden1");
+//       card3.classList.remove("hidden1");
+//     }
+//   });
+
+  
+  
+  
+  
+  
+
+
+
+
+
+// ------------footer------------
+
+
+
+// --------range
+var range = document.getElementById("progretss--main");
